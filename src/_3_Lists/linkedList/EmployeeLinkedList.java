0381994370537d49f76b02f65b82cd20dd328db4
Vit <besteffects@gmail.com>
2020-08-18 16:05:03 -0400

    printList, addToFront

--- src/_3_Lists/linkedList/EmployeeLinkedList.java
+++ src/_3_Lists/linkedList/EmployeeLinkedList.java
@@ -7,9 +7,21 @@ public class EmployeeLinkedList {
     private EmployeeNode head;
 
     //We are inserting the node to the front of the list
+    //we are inserting a node that is set as head, and previous head becomes next node
     public void addToFront(Employee employee) {
         EmployeeNode node = new EmployeeNode(employee);
         node.setNext(head);
         head=node;
+    }
+
+    public void printList(){
+        EmployeeNode current = head;
+        System.out.print("HEAD -> ");
+        while (current!=null){
+            System.out.print(current);
+            System.out.print(" -> ");
+            current=current.getNext(); //move to the next node
+        }
+        System.out.print("null");
     }
 }
--- src/_3_Lists/linkedList/_3_LinkedList.java
+++ src/_3_Lists/linkedList/_3_LinkedList.java
@@ -9,10 +9,16 @@ import java.util.List;
 public class _3_LinkedList {
     public static void main(String[] args) {
 
-        List<Employee> employeeList = new LinkedList<>();
-        employeeList.add(new Employee("Jane", "Jones", 123));
-        employeeList.add(new Employee("John", "Doe", 4567));
-        employeeList.add(new Employee("Mary", "Smith", 22));
-        employeeList.add(new Employee("Mike", "Wilson", 3245));
+
+        Employee janeJones =new Employee("Jane", "Jones", 123);
+        Employee johnDoe= new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
     }
 }
