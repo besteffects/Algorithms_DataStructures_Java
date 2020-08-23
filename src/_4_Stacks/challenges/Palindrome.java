23ba034d7111d846dda27047a6069ffdae2c7d4c
Vit <besteffects@gmail.com>
2020-08-23 19:29:22 -0400

    Palindrome challenge

--- src/_4_Stacks/challenges/Palindrome.java
+++ src/_4_Stacks/challenges/Palindrome.java
@@ -1,5 +1,7 @@
 package _4_Stacks.challenges;
 
+import java.util.LinkedList;
+
 public class Palindrome {
     public static void main(String[] args) {
         // should return true
@@ -14,7 +16,23 @@ public class Palindrome {
         System.out.println(checkForPalindrome("Don't nod"));
     }
 
-    public static boolean checkForPalindrome(String string) {
-        return false;
+    //reverse the string and compare it with the original string
+    static boolean checkForPalindrome(String string) {
+        LinkedList<Character> stack = new LinkedList<Character>();
+        StringBuilder stringNoPunctuation = new StringBuilder(string.length());
+        String lowerCase = string.toLowerCase();
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                stringNoPunctuation.append(c);
+                stack.push(c);
+            }
+        }
+        StringBuilder reversedString = new StringBuilder(stack.size());
+        while (!stack.isEmpty()) {
+            reversedString.append(stack.pop()); //pop characters out and compare them with a stringBuilder
+        }
+        return reversedString.toString().equals(stringNoPunctuation.toString());
     }
+
 }
