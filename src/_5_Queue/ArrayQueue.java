cccc859528d5b21430a6ceb00f1b279ef54d8b4d
Vit <besteffects@gmail.com>
2020-08-24 12:57:12 -0400

    ArrayQueue implementation

--- src/_5_Queue/ArrayQueue.java
+++ src/_5_Queue/ArrayQueue.java
@@ -1,5 +1,7 @@
 package _5_Queue;
 
+import java.util.NoSuchElementException;
+
 public class ArrayQueue {
     private Employee[] queue;
     private int front;
@@ -10,7 +12,7 @@ public class ArrayQueue {
     }
 
     //Add an element to the back of the queue and resize it when array is full
-    public void add(Employee employee) {
+    void add(Employee employee) {
         if (back == queue.length) {
             Employee[] newArray = new Employee[2 * queue.length]; //increse array size twice
             System.arraycopy(queue, 0, newArray, 0, queue.length);
@@ -18,5 +20,37 @@ public class ArrayQueue {
         }
         queue[back] = employee; //assign an employee to the last element in array
         back++;
+    }
+
+    Employee remove() {
+        if (size() == 0) { //1 Check if a queue is empty
+            throw new NoSuchElementException();
+        }
+        Employee employee = queue[front]; //save the employee at the front of the queue
+        queue[front] = null;
+        front++; //front index moves one position ahead
+
+        if (size() == 0) { //for the case when only one employee is being removed
+            front = 0;
+            back = 0;
+        }
+        return employee; //Return employee at the front of the queue
+    }
+
+    Employee peek() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+        return queue[front]; //get the front element
+    }
+
+    public int size() {
+        return back - front;
+    }
+
+    void printQueue() {
+        for (int i = front; i < back; i++) { //back refers to the next available position so the length of the queue is back-1
+            System.out.println(queue[i]);
+        }
     }
 }
--- src/_5_Queue/Main.java
+++ src/_5_Queue/Main.java
@@ -1,4 +1,40 @@
 package _5_Queue;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayQueue queue = new ArrayQueue(10);
+        System.out.println("Adding elements to queue: ");
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.add(marySmith);
+        queue.add(mikeWilson);
+        queue.add(billEnd);
+        queue.printQueue();
+
+        queue.remove();
+        queue.remove();
+        System.out.println("Removed an element from queue: ");
+        queue.printQueue();
+
+        System.out.println("Peek: " + queue.peek());
+        System.out.println("Queue is not changed after peek(): ");
+        queue.printQueue();
+
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        System.out.println("Deleted all 3 elements left. Size is:  " + queue.size());
+
+        // System.out.println("Removing an element from empty queu: " +queue.remove());
+        queue.add(mikeWilson);
+        queue.printQueue();
+    }
 }
